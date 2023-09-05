6ecd7bec89f37584ec9420694110535f13049b62
wilfredo-domanico-jr <wilfredo.domanico.jr@gmail.com>
2023-09-05 17:52:46 +0800

    Final Website for Capstone Defense

---
+++ Company/constants/enable-cert.php
@@ -0,0 +1,9 @@
+                        <center>
+                        
+                        <button class ="btn btn-success" data-toggle="modal" data-target="#certificateModal<?php echo $row['member_no']; ?>">
+                        
+                            <i class="fas fa-certificate"></i>
+                            Give Certificate
+                       
+                        </button>
+                    </center>
---
+++ constants/settings.php
@@ -0,0 +1,11 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = '';
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = '';
+
+?>
